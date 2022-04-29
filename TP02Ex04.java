db97ec4ca9ae39f9c9d961ccbbce8efa01273330
diegonegrete <102666925+diegonegrete@users.noreply.github.com>
2022-04-29 18:19:11 -0300

    Add files via upload
    Diego Negrete  CB1740229 ROBERTO RODRIGUES DA SILVA JUNIOR CB3000419

---
+++ TP02Ex04.java
@@ -0,0 +1,33 @@
+//Diego Negrete  CB1740229 ROBERTO RODRIGUES DA SILVA JUNIOR CB3000419
+
+//4. Armazenar seis valores em uma matriz de ordem 2x3. Apresentar os valores na tela.
+
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class TP02Ex04 {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int[][] v = new int[2][3];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                out.printf("Informe [" + i + "][" + j + "]: ");
+                v[i][j] = scan.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                out.print("[" + v[i][j] + "] ");                
+            }
+            out.println();
+        }           
+    }
+}
